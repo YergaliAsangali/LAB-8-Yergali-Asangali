a2bf2e2671f83470dd2d2e648788a1196883a8c8
YergaliAsangali <YergaliAsangali@users.noreply.github.com>
2022-05-06 19:11:43 +0600

    Test
    Test

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import './Footer.css'
+
+const Footer = () =>{
+    return (
+        <div className='Footer'>Footerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
+            
+        </div>
+
+    );
+}
+
+export default Footer;
